2fa3fb977163c62607938ec60aa8960ad3387ff0
silva3265 <silva3265@gmail.com>
2024-05-17 12:43:43 -0300

    Usando listas com Generics

--- src/hotelaria/Principal.java
+++ src/hotelaria/Principal.java
@@ -4,23 +4,24 @@ import java.util.ArrayList;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		CadastroHotel cadastro = new CadastroHotel();
-		cadastro.adicionar("Jaguaribe Lodge", "Fortim/CE", 1300);
-		cadastro.adicionar("Vila Selvagem", "Fortim/CE", 1400);
-		cadastro.adicionar("Hotel Fazenda Dona Carolina", "Itatiba/SP", 2200);
-		cadastro.adicionar("Tivoli Ecoresort", "Praia do Forte/BA", 2000);
-		cadastro.adicionar("Mercure", "Uberlândia/MG", 400);
+    public static void main(String[] args) {
+        CadastroHotel cadastro = new CadastroHotel();
+        cadastro.adicionar("Jaguaribe Lodge", "Fortim/CE", 1300);
+        cadastro.adicionar("Vila Selvagem", "Fortim/CE", 1400);
+        cadastro.adicionar("Hotel Fazenda Dona Carolina", "Itatiba/SP", 2200);
+        cadastro.adicionar("Tivoli Ecoresort", "Praia do Forte/BA", 2000);
+        cadastro.adicionar("Mercure", "Uberlândia/MG", 400);
 
-		ArrayList hoteis = cadastro.obterTodos();
-		imprimirHoteis(hoteis);
-	}
+        ArrayList<Hotel> hoteis = cadastro.obterTodos();
+        imprimirHoteis(hoteis);
+    }
 
-	private static void imprimirHoteis(ArrayList hoteis) {
-		for (int i = 0; i < hoteis.size(); i++) {
-			Hotel hotel = (Hotel) hoteis.get(i); // vai obter o objeto daquele indice
-			System.out.printf("%s (%s) -> %.2f%n", hotel.getNome(), hotel.getCidade(), hotel.getPrecoDiaria());
-		}
-	}
+    private static void imprimirHoteis(ArrayList<Hotel> hoteis) {
+        for (int i = 0; i < hoteis.size(); i++) {
+            Hotel hotel = hoteis.get(i);
+            System.out.printf("%s (%s) -> %.2f%n", hotel.getNome(),
+                    hotel.getCidade(), hotel.getPrecoDiaria());
+        }
+    }
 
 }
