4a47fd17467dc4717eb144d4809c6a35238ac459
silva3265 <silva3265@gmail.com>
2024-05-22 13:42:16 -0300

    LinkedList: mais performance na adição e remoção

--- src/megasena/Sorteador.java
+++ src/megasena/Sorteador.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 public class Sorteador implements Iterable<Integer> { /* vai implementar diretamente o Iterable */
 
     @Override
-    public Iterator<Integer> iterator() { /* como estamo impementando uma interface temos que implementar os metodos */
+    public Iterator<Integer> iterator() { /* como estamos impementando uma interface temos que implementar os metodos */
         return new SorteadorIterator();
     }
 
