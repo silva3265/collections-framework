8a3c589ec4b26bce8ba6625cf63d961772110e9b
silva3265 <silva3265@gmail.com>
2024-05-28 15:31:31 -0300

    Testando a implementação de hashCode com HashSet

--- src/collections_set/Principal.java
+++ src/collections_set/Principal.java
@@ -1,22 +1,30 @@
 package collections_set;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Principal {
 
-    public static void main(String[] args) {
-        Contato contato1 = new Contato("Maria", "maria@algaworks.com", 40);
-        Contato contato2 = new Contato("José", "jose@algaworks.com", 30);
-        Contato contato3 = new Contato("João", "joao@algaworks.com", 25);
-        Contato contato4 = new Contato("João da Silva", "joao@algaworks.com", 80);
+	public static void main(String[] args) {
+		Set<Contato> contatos = new HashSet<>();
 
-        System.out.println(contato1.hashCode());
-        System.out.println(contato2.hashCode());
-        System.out.println(contato3.hashCode());
-        System.out.println(contato4.hashCode());
+		System.out.println("---");
+		contatos.add(new Contato("Maria", "maria@algaworks.com", 40));
+		contatos.add(new Contato("Ana", "ana@algaworks.com", 30));
+		contatos.add(new Contato("José", "jose@algaworks.com", 25));
+		contatos.add(new Contato("Rosa", "rosa@algaworks.com", 50));
+		contatos.add(new Contato("João", "joao@algaworks.com", 70));
+		System.out.println("--");
+		contatos.add(new Contato("Josefina", "josefina@algaworks.com", 70));
+		System.out.println("--");
+		contatos.add(new Contato("Josefina", "josefina@algaworks.com", 70));
+		contatos.add(null);
+		System.out.println("--");
+ 
+//        System.out.println(contatos);
 
-//        System.out.println(contato2.equals(contato3)); // false
-//        System.out.println(contato2.hashCode() == contato3.hashCode()); /* vai retornar verdadeiro porque no hashcode tem a logica para comparar os emails */
-    }
-     	/* se o equals é igual, o hashcode tambem tem que ser igual, é uma boa pratica */
-    	/* sempre implementa o hashcode usando como base  a mesma propriedade que usamos para implementar o metodo euquals */
+		boolean resultado = contatos.contains(new Contato("Alaor", "alaor@algaworks.com", 30)); /* contains - vai verificar se tem na lista */
+		System.out.println(resultado);
+	}
 
 }
